0d7dfdca08fe558a32be6adae1762974830a7b8a
あや <ymymym.a2@gmail.com>
2015-03-02 16:54:23 +0900

    画像表示！

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -2,9 +2,9 @@ class AboutController < ApplicationController
   def index
   	@favorite_recipes = Favorite.favorite_count.map{|recipe_id| Recipe.find_by(id: recipe_id)}
   	@todays_menu = Menu.find(:last)
-  	@menu_recipes = MenuRecipe.where(menu_id: 2) 
+  	@menu_recipes = MenuRecipe.where(menu_id: 3) 
   	@recipes = @menu_recipes.map{|menu_recipe| menu_recipe.recipe}
-  	@nurtrition_menu = Menu.find(2)
+  	@nurtrition_menu = Menu.find(3)
   	
   end
 end
