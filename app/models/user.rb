28a93c3499cbdd34d28cc9f077e0ed5b642c8bb9
あや <ymymym.a2@gmail.com>
2015-03-09 04:33:34 +0900

    修正！

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,20 +19,7 @@ class User < ActiveRecord::Base
   has_many :favorites
   has_many :favorite_recipes, through: :favorites, source: :recipe
   has_many :favorite_menus, through: :favorites, source: :menu
-
-
-  # has_attached_file :image,
-  #                   # :styles => {
-  #                   #     :thumb  => "100x100",
-  #                   #     :medium => "200x200",
-  #                   #     :large => "600x400"
-  #                   # },
-  #                   :storage => :s3,
-  #                   :s3_permissions => :private,
-  #                   :s3_credentials => "#{Rails.root}/config/s3.yml",
-  #                   :path => ":attachment/:id/:style.:extension"
  
-  # validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
  
   # def authenticated_image_url(style)
   #   image.s3_object(style).url_for(:read, :secure => true)
