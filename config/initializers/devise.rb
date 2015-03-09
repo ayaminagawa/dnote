9c556921742aab387a635103f8fef2b1f8d092f5
あや <ymymym.a2@gmail.com>
2015-03-09 15:12:30 +0900

    ！

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,6 @@ Devise.setup do |config|
     config.omniauth :twitter,  "v7ByEftCPcYL06ufERUZ6IDrZ", "Py7aflkqjv83zB9fWf54RGiSy1q97BafdPAV6Mie8uwoGmvy5y", {:image_size => ‘original’, :display => ‘popup’} )
   end
 
-
  # if Rails.env.production?
  #    config.omniauth( :facebook,
  #                   “296639803850370”,
