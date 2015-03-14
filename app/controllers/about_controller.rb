59b19f82296e4b51c4d2a081895dff5c74dcc46b
あや <ymymym.a2@gmail.com>
2015-03-15 04:01:57 +0900

    おすすめレシピのidを４にした

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -2,7 +2,7 @@ class AboutController < ApplicationController
   def index
   	@favorite_recipes = Favorite.favorite_count.map{|recipe_id| Recipe.find_by(id: recipe_id)}
   	@todays_menu = Menu.find(:last)
-  	@menu_recipes = MenuRecipe.where(menu_id: 3) 
+  	@menu_recipes = MenuRecipe.where(menu_id: 4) 
   	@recipes = @menu_recipes.map{|menu_recipe| menu_recipe.recipe}
   	@nurtrition_menu = Menu.find(3)
   end
