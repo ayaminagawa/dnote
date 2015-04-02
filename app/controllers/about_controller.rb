a9420ffc929c325fd9cff0e4799c409f32a66f7a
あや <ymymym.a2@gmail.com>
2015-04-02 15:31:39 +0900

    トップページでバック

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -2,15 +2,18 @@ class AboutController < ApplicationController
   def index
   	@favorite_recipes = Favorite.favorite_count.map{|recipe_id| Recipe.find_by(id: recipe_id)}
   	@todays_menu = Menu.find(:last)
-  	@menu_recipe = MenuRecipe.where(menu_id: @todays_menu) 
-  	@main_recipe = Recipe.find(@menu_recipe.map{|menu_recipe| menu_recipe.main})
-    @side1_recipe = Recipe.find(@menu_recipe.map{|menu_recipe| menu_recipe.side1})
-    @side2_recipe = Recipe.find(@menu_recipe.map{|menu_recipe| menu_recipe.side2})
-    @main_recipe = @main_recipe.first
-    @side1_recipe = @side1_recipe.first
-    @side2_recipe = @side2_recipe.first
+
+  	@menu_recipe = MenuRecipe.find_by(menu_id: @todays_menu.id)
+
+  	@main_recipe = Recipe.find(@menu_recipe.main)
+    @side1_recipe = Recipe.find(@menu_recipe.side1)
+    @side2_recipe = Recipe.find(@menu_recipe.side2)
+    # @main_recipe = @main_recipes.first
+    # @side1_recipe = @side1_recipes.first
+    # @side2_recipe = @side2_recipes.first
   	@nurtrition_menu = Menu.find(3)
     @column = Column.find(:last)
+
 
   	require "date"
 
