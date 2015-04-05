bda8f4aab8beabf4a88d5b5a8013afc5ab8c2548
あや <ymymym.a2@gmail.com>
2015-04-05 15:03:23 +0900

    layout リファクタリング修正

--- app/controllers/nutritionists/sessions_controller.rb
+++ app/controllers/nutritionists/sessions_controller.rb
@@ -1,12 +1,3 @@
 class Nutritionists::SessionsController < Devise::SessionsController
- 
-  def new
-    super
-    render :layout => "preview_layout"
-  end
- 
-  def create
-    super
-    render :layout => "preview_layout"
-  end
+
 end
