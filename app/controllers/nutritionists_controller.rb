e4e25d3aad4936633be465fc969f4c3da098e425
あや <ymymym.a2@gmail.com>
2015-04-24 09:03:28 +0900

    とちゅう！

--- app/controllers/nutritionists_controller.rb
+++ app/controllers/nutritionists_controller.rb
@@ -1,5 +1,7 @@
 class NutritionistsController < ApplicationController
   def show
+    @id = params[:id]
+    @nutritionist = Nutritionist.find(@id)
     @columns = Column.where(:nutritionist_id => current_nutritionist.id)
   end
 
