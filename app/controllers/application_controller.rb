a27ea03d368fe548b93dce98625721967f32c4e8
あや <ymymym.a2@gmail.com>
2015-05-01 15:42:43 +0900

    ログインした後mypageにリダイレクトされるようにした！

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    if params[:controller] == "nutritionists/registrations" || params[:controller] == "nutritionists/sessions"
+       nutritionist_path(current_nutritionist)
+      else
+       user_path(current_user)
+    end
+   
+  end
   
   protected
   
