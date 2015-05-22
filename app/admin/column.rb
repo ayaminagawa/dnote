78eb674c69a1771f5cde1b466e7d500c69c278b1
あや <ymymym.a2@gmail.com>
2015-05-23 00:14:25 +0900

    column編集の許可

--- app/admin/column.rb
+++ app/admin/column.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Column do
 
-  permit_params :permission
+  permit_params :permission, :user_id
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
