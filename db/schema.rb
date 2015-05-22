b8b841f7177c376778ec989052ba349e7e0855d1
あや <ymymym.a2@gmail.com>
2015-05-22 09:23:34 +0900

    active adminできた！

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150519095650) do
+ActiveRecord::Schema.define(version: 20150509065703) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -24,9 +24,9 @@ ActiveRecord::Schema.define(version: 20150519095650) do
     t.datetime "updated_at"
   end
 
-  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
-  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
-  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
+  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace"
+  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
 
   create_table "admin_users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -43,8 +43,8 @@ ActiveRecord::Schema.define(version: 20150519095650) do
     t.datetime "updated_at"
   end
 
-  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
-  add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
+  add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
 
   create_table "category_selects", force: true do |t|
     t.integer  "recipe_id"
@@ -90,7 +90,7 @@ ActiveRecord::Schema.define(version: 20150519095650) do
     t.datetime "updated_at"
   end
 
-  add_index "favorites", ["user_id", "recipe_id"], name: "index_favorites_on_user_id_and_recipe_id", unique: true, using: :btree
+  add_index "favorites", ["user_id", "recipe_id"], name: "index_favorites_on_user_id_and_recipe_id", unique: true
 
   create_table "feedbacks", force: true do |t|
     t.integer  "user_id"
@@ -132,7 +132,7 @@ ActiveRecord::Schema.define(version: 20150519095650) do
     t.integer  "side2"
   end
 
-  add_index "menu_recipes", ["recipe_id", "menu_id"], name: "index_menu_recipes_on_recipe_id_and_menu_id", unique: true, using: :btree
+  add_index "menu_recipes", ["recipe_id", "menu_id"], name: "index_menu_recipes_on_recipe_id_and_menu_id", unique: true
 
   create_table "menus", force: true do |t|
     t.datetime "created_at"
@@ -169,8 +169,8 @@ ActiveRecord::Schema.define(version: 20150519095650) do
     t.datetime "image_updated_at"
   end
 
-  add_index "nutritionists", ["email"], name: "index_nutritionists_on_email", unique: true, using: :btree
-  add_index "nutritionists", ["reset_password_token"], name: "index_nutritionists_on_reset_password_token", unique: true, using: :btree
+  add_index "nutritionists", ["email"], name: "index_nutritionists_on_email", unique: true
+  add_index "nutritionists", ["reset_password_token"], name: "index_nutritionists_on_reset_password_token", unique: true
 
   create_table "procedures", force: true do |t|
     t.integer  "recipe_id"
@@ -210,30 +210,27 @@ ActiveRecord::Schema.define(version: 20150519095650) do
     t.datetime "updated_at"
     t.string   "email"
     t.string   "image"
-    t.string   "encrypted_password",       default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",            default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "uid",                      default: "", null: false
-    t.string   "provider",                 default: "", null: false
+    t.string   "uid",                    default: "", null: false
+    t.string   "provider",               default: "", null: false
     t.string   "name"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.string   "password_digest"
-    t.integer  "nutritionist_id"
-    t.string   "nutritionist_description"
-    t.string   "permission"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider"
 
 end
