88b4219de8831f4d8755b8f4581aa781d29c6f68
あや <ymymym.a2@gmail.com>
2015-05-28 23:04:35 +0900

    画像の縮尺を変えずに表示

--- app/models/made_report.rb
+++ app/models/made_report.rb
@@ -1,5 +1,5 @@
 class MadeReport < ActiveRecord::Base
-	has_attached_file :image, styles: { original: "1920x1680>",medium: "300x300>", thumb: "100x100>" }, default_url: "/system/missing/:style/missing.jpg"
+	has_attached_file :image, styles: { original: "1920x1680#",medium: "300x300>", thumb: "100x100>" }, default_url: "/system/missing/:style/missing.jpg"
 
  validates_attachment :image, presence: true,
  content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ActiveRecord::Base
   #   image.s3_object(style).url_for(:read, :secure => true)
   # end
 
-  has_attached_file :image, styles: { original: "1920x1680>",medium: "300x300>", thumb: "100x100>" },:url => "/images/:class/:attachment/:id_partition/:style/:filename", default_url: "/system/missing/:style/missing.jpg"
+  has_attached_file :image, styles: { original: "1920x1680#",medium: "300x300>", thumb: "100x100>" },:url => "/images/:class/:attachment/:id_partition/:style/:filename", default_url: "/system/missing/:style/missing.jpg"
 
   validates_attachment :image, 
   content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
