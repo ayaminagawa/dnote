d705d86bd84d4a206a1c8d258ce65ef4c9c5bb5c
muraikenta <muraikenta0507@gmail.com>
2015-08-30 16:16:33 +0900

    csvの作成

--- lib/tasks/food_info_task.rb
+++ lib/tasks/food_info_task.rb
@@ -6,29 +6,46 @@ require 'nokogiri'
 class Tasks::FoodInfoTask
   def self.import
 
-    food_info = FoodInfo.new
     columns = FoodInfo.column_names
 
-    url = 'http://fooddb.mext.go.jp/details/details.pl?ITEM_NO=11_11198_6'
-    charset = nil
+    i = 1001
 
-    html = open(url) do |f|
-      charset = f.charset # 文字種別を取得
-      f.read # htmlを読み込んで変数htmlに渡す
+    while i <= 18016 do
+      food_info = FoodInfo.new
+
+      if i < 10000
+        url = "http://fooddb.mext.go.jp/details/details.pl?ITEM_NO=10_0#{i}_6"
+      else
+        url = "http://fooddb.mext.go.jp/details/details.pl?ITEM_NO=10_#{i}_6"
+      end
+      charset = nil
+
+      html = open(url) do |f|
+        charset = f.charset # 文字種別を取得
+        f.read # htmlを読み込んで変数htmlに渡す
+      end
+
+      if html.match(/ERROR/)
+        i = i + 1000 - (i % 1000) + 1
+        puts "next!!"
+        next
+      end
+
+      doc = Nokogiri::HTML.parse(html, nil, charset)
+
+      food_info.item_no = doc.css(".food_name").first.text.match(/：\[(.*?)\]/)[1].to_i
+      # food_info.name = doc.css(".food_name").first.text.gsub(/\[食品番号\]/, '')
+      food_info.name = doc.css(".food_name").first.text
+
+      doc.css("td.num, td.marker").each_with_index do |node, index|
+        eval("food_info.#{columns[index + 3]} = node.inner_text.to_f")
+      end
+
+      food_info.save
+      puts "item_no: #{food_info.item_no}"
+      i += 1
     end
 
-    doc = Nokogiri::HTML.parse(html, nil, charset)
-
-    food_info.item_no = doc.css(".food_name").first.text.match(/：\[(.*?)\]/)[1].to_i
-    # food_info.name = doc.css(".food_name").first.text.gsub(/\[食品番号\]/, '')
-    food_info.name = doc.css(".food_name").first.text
-
-    doc.css("td.num, td.marker").each_with_index do |node, index|
-      p node.text
-      eval("food_info.#{columns[index + 3]} = node.inner_text.to_f")
-    end
-
-    food_info.save
 
 
   end
